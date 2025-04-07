4179107fa18c4020f1ee0ec9f33923b7b0ee1e64
Stepan Martyushov <techseth@yandex.ru>
2025-04-07 18:57:09 +0300

    Update epsteinframe.py

--- epsteinframe.py
+++ epsteinframe.py
@@ -17,22 +17,29 @@ class MainUI(QMainWindow):
         # Список конфигураций
         self.cBox_conf.addItems(['1','2','3'])
         # Подключаем обработчики событий
-        self.pBtn_init.clicked.connect(self.init_pico)
+        # self.pBtn_init.clicked.connect(self.init_pico)
         self.pBtn_start.clicked.connect(self.start)
         self.pBtn_save.clicked.connect(self.save)
+        
+        #график
+        self.graphWidget.setBackground('w')
 
     def init_pico(self):
+        '''- Инициализация осциллографа Picoscope3204D и подключение портов -'''
         self.picoscope = Picoscope3204D()
         self.picoscope.initialize_ports()
 
     def start(self):
+        '''- Измерения -'''
         # При нажатии кнопки "Начать измерения" запускаем цикл измерений
         print("Начало измерений")
-        self.freq = int(self.lEd_f.text().replace(',','.')) # частота, Гц
+        # Рабочая частота и целевая индукция задаются пользователем в интерфейсе
+        self.freq = int(self.lEd_f.text().replace(',','.')) # частота генератора, Гц
         timebase=1252 # timebase=1252 == 10 мкс
-        B = float(self.lEd_B.text().replace(',','.')) # целевая индукция, Тл
+        B = float(self.lEd_B.text().replace(',','.')) # магнитная индукция, Тл
         configNumber = int(self.cBox_conf.currentText()) # номер конфигурации катушек на выбор 1 из 3, выбор из списка
 
+        # Параметры пластин из интерфейса задаются пользователем
         x = float(self.lEd_x.text().replace(',','.'))/1000 # толщина, мм в м
         y = float(self.lEd_y.text().replace(',','.'))/1000 # ширина, мм в м
         N = int(self.lEd_N.text().replace(',','.')) # количество слоев полос, уложенных в рамку
@@ -42,6 +49,7 @@ class MainUI(QMainWindow):
         key = 0 # соответствие измеренной Вм и заданной Вм меняется на 1 по достижении заданной величины индукции, запускает измерение потерь.
 
         try:
+            # цикл увеличения амплитуды генератора до достижения целевой индукции
             while not key:
                 self.amp += 100000
                 self.picoscope.setup_generator(self.freq, amplitude=self.amp)
@@ -49,12 +57,18 @@ class MainUI(QMainWindow):
                 samples = int(5*100000/self.freq)
                 self.data = self.picoscope.read_data(max_samples=samples, sample_rate=timebase)
 
+                # Проверка выхода за пределы каналов
+                self.
+
                 sampleParameters = [x, y, N, ro]
                 runParameters = [self.data, self.freq, key, configNumber, sampleParameters]
-                Bmax = epscalc.run(runParameters)[0]
+                result = epscalc.run(runParameters)
+                Bmax = result[0]
+                
                 print(Bmax)
-                if Bmax >= B:
+                if Bmax >= B or self.amp >= 2000000:
                     key = 1
+                
 
             self.picoscope.setup_generator(self.freq, amplitude=self.amp)
             samples = int(30*100000/self.freq)
@@ -64,13 +78,14 @@ class MainUI(QMainWindow):
             runParameters = [self.data, self.freq, key, configNumber, sampleParameters]
             result = epscalc.run(runParameters)
             self.Bmax = result[0]
+            self.H = result[1]
+            self.B = result[2]
             self.powerLosses = result[3]
-
+            
             print(self.Bmax, self.powerLosses)
+            self.plotData()
         except Exception as e:
             print(f"Что-то пошло не так: {e}")
-        finally:
-            self.picoscope.close()
 
     def save(self):
         # Обработка нажатия кнопки "Сохранить результат"
@@ -80,13 +95,29 @@ class MainUI(QMainWindow):
         except Exception as e:
             print(f"Что-то пошло не так: {e}")
 
+    def plotData(self):
+        self.graphWidget.clear()       
+        styles = {'color': 'black', 'font-size': '12px'}
+        self.graphWidget.setLabel('left', "B", **styles)
+        self.graphWidget.setLabel('bottom', "H", **styles)
+        self.graphWidget.showGrid(x = True, y = True)
+        self.graphWidget.setXRange(min(self.H), max(self.H))
+        self.graphWidget.setYRange(min(self.B), max(self.B))
+        self.pltData = self.graphWidget.plot(x = self.H, y = self.B, pen = 'b')#, symbol = 'o')
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainUI()
-    window.show()
-    sys.exit(app.exec_())
+    epstein = MainUI()
+    epstein.show()
+
     try:
-        self.picoscope.close()
-        print(f"pico closed")
+        epstein.init_pico()
+        sys.exit(app.exec_())
     except Exception as e:
         print(f"An error occurred: {e}")
+    finally:
+        epstein.picoscope.close()
+        print(f"pico closed")
+
+
