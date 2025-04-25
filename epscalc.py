989102e2304777a576ee3be438a7c20dd59aea5c
iPtrw <i.petrov@amtc.org>
2025-04-25 16:28:31 +0300

    Update epscalc.py

--- epscalc.py
+++ epscalc.py
@@ -24,7 +24,7 @@ def run (runParameters):
     
     coilCoef = 4*measuringCoilCount # 4 измерительных катушки в рамке 
     x, y, N, ro = test_parameters
-    currentCoef = 1.5 #
+    currentCoef = 5 #
     ersted2Am = 9.8*79.57 #пересчет из Эрстед в А/м, 9.8 - множитель для конкретного набора катушек
     match configNumber:
         case 1: #150:50
